279d3095825574a0b73e19d15ac2c45c708374a0
Dave Lively <davelively14@gmail.com>
2017-01-19 22:42:27 -0500

    Checkpoint 3 complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,29 @@
-angular.module('blocJams', []);
+(function() {
+  function config($stateProvider, $locationProvider) {
+
+    // Angular core component
+    $locationProvider
+      .html5Mode({
+        // Avoids setting a base
+        enabled: true,
+
+        // requireBase: false will disable hashbangs (#!)
+        requireBase: false
+      });
+      
+    // UI-Router core component
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: '/templates/landing.html'
+      })
+      .state('album', {
+        url: '/album',
+        templateUrl: '/templates/album.html'
+      });
+  }
+
+  angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
+})();
