0d37323e44a00c53232fd026c880fcc55168ebb4
Dave Lively <davelively14@gmail.com>
2017-01-23 19:22:08 -0500

    Assignment 11 complete

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -2,24 +2,7 @@
   function timecode() {
 
     return function(seconds) {
-      var seconds = Number.parseFloat(seconds);
-
-      if (Number.isNaN(seconds)) {
-        return '-:--';
-      }
-
-      var minutes = Math.floor(seconds / 60);
-      var remainingSeconds = Math.floor(seconds % 60);
-
-      var output = minutes + ':';
-
-      if (remainingSeconds < 10) {
-        output += '0';
-      }
-
-      output += remainingSeconds;
-
-      return output;
+      return buzz.toTimer(seconds);
     };
 
   }
