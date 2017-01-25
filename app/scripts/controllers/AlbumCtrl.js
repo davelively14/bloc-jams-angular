c46d24ceae1357e1858de0c6a69935dbb7e74e0e
Dave Lively <davelively14@gmail.com>
2017-01-25 12:36:17 -0500

    Extra Credit 6 complete - stars

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,19 @@
 (function() {
 
-  function AlbumCtrl(Fixtures, SongPlayer) {
-    this.albumData = Fixtures.getAlbum();
-    this.songPlayer = SongPlayer;
+  function AlbumCtrl(Fixtures, SongPlayer, $scope) {
+    $scope.albumData = Fixtures.getAlbum();
+    $scope.songPlayer = SongPlayer;
+
+    $scope.setRating = function(value) {
+      Fixtures.rateAlbum($scope.albumData.id, value);
+    };
+
+    $scope.setSongRating = function(i, value) {
+      Fixtures.rateSong($scope.albumData.id, i, value);
+    };
   }
 
   angular
     .module('blocJams')
-    .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
+    .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', '$scope', AlbumCtrl]);
 })();
--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -4,33 +4,37 @@
     var Fixtures = {};
 
     var albumPicasso = {
+      id: 0,
       title: 'The Colors',
       artist: 'Pablo Picasso',
       label: 'Cubism',
       year: '1881',
       albumArtUrl: '/assets/images/album_covers/01.png',
       songs: [
-        { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
-        { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
-        { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
-        { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
-        { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
-      ]
+        { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue', rating: null },
+        { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green', rating: null },
+        { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red', rating: null },
+        { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink', rating: null },
+        { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta', rating: null }
+      ],
+      rating: 0
     };
 
     var albumMarconi = {
+      id: 1,
       title: 'The Telephone',
       artist: 'Guglielmo Marconi',
       label: 'EM',
       year: '1909',
       albumArtUrl: '/assets/images/album_covers/20.png',
       songs: [
-        { title: 'Hello, Operator?', duration: '1:01' },
-        { title: 'Ring, ring, ring', duration: '5:01' },
-        { title: 'Fits in your pocket', duration: '3:21' },
-        { title: 'Can you hear me now?', duration: '3:14' },
-        { title: 'Wrong phone number', duration: '2:15' }
-      ]
+        { title: 'Hello, Operator?', duration: '1:01', rating: null },
+        { title: 'Ring, ring, ring', duration: '5:01', rating: null },
+        { title: 'Fits in your pocket', duration: '3:21', rating: null },
+        { title: 'Can you hear me now?', duration: '3:14', rating: null },
+        { title: 'Wrong phone number', duration: '2:15', rating: null }
+      ],
+      rating: 0
     };
 
     Fixtures.getAlbum = function() {
@@ -39,6 +43,25 @@
 
     Fixtures.getAllAlbums = function() {
       return [albumPicasso, albumMarconi];
+    };
+
+    Fixtures.rateAlbum = function(albumId, rating) {
+      var allAlbums = Fixtures.getAllAlbums();
+
+      var album = allAlbums.find(function(x) {
+        return x.id == albumId;
+      });
+      album.rating = rating;
+    };
+
+    Fixtures.rateSong = function(albumId, songIndex, rating) {
+      var allAlbums = Fixtures.getAllAlbums();
+
+      var album = allAlbums.find(function(x) {
+        return x.id == albumId;
+      });
+
+      album.songs[songIndex].rating = rating;
     };
 
     Fixtures.getCollection = function(numberOfAlbum) {
